9f96549bdff25dac1c8bfb53bd9e9b6d7e873dbf
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-05 22:32:22 +0100

    component progress bar added

---
+++ src/component/ProgressBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProgressBar.css';
+
+const ProgressBar = (params) => (
+  <div className='wrapperProgress'>
+    <div className='progressBar'></div>
+  </div>
+);
+
+export default ProgressBar;
